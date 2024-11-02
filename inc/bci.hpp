a3c0150f98535cec7d8dc846a60943bc65af9e7c
Mihai-Simedrea <mihai.nicolae.simedrea@gmail.com>
2024-11-02 17:43:39 +0200

    feat: added the BCI class template

---
+++ inc/bci.hpp
@@ -0,0 +1,15 @@
+#ifndef BCI_HPP
+#define BCI_HPP
+
+class BCI {
+    public: 
+        BCI();
+        ~BCI();
+        void addDevice(); // todo
+        void process(); // todo
+
+    private:
+        // todo
+};
+
+#endif // BCI_HPP
